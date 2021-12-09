c603d370da137483e6ac866e56bac8c0ec3e0669
Zoldn <mihail-svetkin@mail.ru>
2021-12-09 21:06:00 +0300

    Create repo. gitignore setup

---
+++ TUnderdark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TUnderdark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
