5bf1a7e4921390db6f19d4df1c0d08e5600b6d0d
Zoldn <mihail-svetkin@mail.ru>
2021-12-11 16:05:35 +0300

    Market zone coordinates

---
+++ TUnderdark/TTSParser/MarketZonePositions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TUnderdark.TTSParser
+{
+    internal static class MarketZonePositions
+    {
+        public static (double X1, double X2, double Z1, double Z2) MarketZone =>
+            (-24.6, -15.5d, -21.0d, 21.0d);
+        public static (double X1, double X2, double Z1, double Z2) Common =>
+            (-34.0d, -24.5, -21.0d, 0.0d);
+        public static (double X1, double X2, double Z1, double Z2) Deck =>
+            (-34.0d, -24.5, 13.5d, 21.0d);
+        public static (double X1, double X2, double Z1, double Z2) Devoured =>
+            (-34.0d, -24.5, 6.0d, 13.4d);
+    }
+}
