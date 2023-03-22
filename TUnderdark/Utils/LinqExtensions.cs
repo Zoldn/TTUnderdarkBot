4992953a5365810fea02b60e48cbb0c5a1e9c37e
Zoldn <mihail-svetkin@mail.ru>
2023-03-22 21:29:02 +0300

    Future value from cities

--- TUnderdark/Utils/LinqExtensions.cs
+++ TUnderdark/Utils/LinqExtensions.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TUnderdark.Utils
 {
-    internal static class DictionaryExtension
+    public static class DictionaryExtension
     {
         public static void Deconstruct<T1, T2>(this KeyValuePair<T1, T2> tuple, out T1 key, out T2 value)
         {
@@ -20,6 +20,46 @@ namespace TUnderdark.Utils
         {
             key = grouping.Key;
             values = grouping;
+        }
+
+        public static T ArgMax<T>(this IEnumerable<T> x, Func<T, double> selector)
+        {
+            if (!x.Any())
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            var ret = x.First();
+
+            foreach (var item in x)
+            {
+                if (selector(item) > selector(ret))
+                {
+                    ret = item;
+                }
+            }
+
+            return ret;
+        }
+
+        public static T ArgMin<T>(this IEnumerable<T> x, Func<T, double> selector)
+        {
+            if (!x.Any())
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            var ret = x.First();
+
+            foreach (var item in x)
+            {
+                if (selector(item) < selector(ret))
+                {
+                    ret = item;
+                }
+            }
+
+            return ret;
         }
     }
 }
--- UnderdarkAI/AI/TargetFunctions/FutureScoreTargetFunction.cs
+++ UnderdarkAI/AI/TargetFunctions/FutureScoreTargetFunction.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TUnderdark.Model;
+using TUnderdark.TTSParser;
 using UnderdarkAI.AI.RotationEstimator;
 using UnderdarkAI.Context;
+using TUnderdark.Utils;
 
 namespace UnderdarkAI.AI.TargetFunctions
 {
@@ -26,9 +28,35 @@ namespace UnderdarkAI.AI.TargetFunctions
             var currentResults = basicScoreFunction.GetScores(board, turn)
                 .ToDictionary(kv => kv.Color, kv => kv.Score);
 
+            Dictionary<Color, double> futureScore = DeckFutureScoreEstimate(board, turn, estimator);
+
+            foreach (var (color, value) in futureScore)
+            {
+                currentResults[color] += value;
+            }
+
+            //Dictionary<Color, double> townPresentScore = TownPresentScoreEstimate(board, turn, estimator);
+
+            //foreach (var (color, value) in townPresentScore)
+            //{
+            //    currentResults[color] += value;
+            //}
+
+            Dictionary<Color, double> townFutureScore = TownFutureScoreEstimate(board, turn, estimator);
+
+            foreach (var (color, value) in townFutureScore)
+            {
+                currentResults[color] += value;
+            }
+
+            return VPScoreTargetFunction.GetDifferenceWithClosestOpponent(currentResults, board, turn);
+        }
+
+        private Dictionary<Color, double> DeckFutureScoreEstimate(Board board, Turn turn, BaseRotationEstimator estimator)
+        {
             Dictionary<Color, double> futureScore = board.Players
                 .ToDictionary(
-                    kv => kv.Key, 
+                    kv => kv.Key,
                     kv => 0.0d
                     );
 
@@ -51,7 +79,7 @@ namespace UnderdarkAI.AI.TargetFunctions
                 var totalCardValueForRotation = totalCards
                     .Sum(c => context.CardsStatsDict[c.SpecificType].BaseValuePerTurn);
 
-                var rotations = estimator.CalculateRotations(board, turn, color, 
+                var rotations = estimator.CalculateRotations(board, turn, color,
                     promotes: promotes, devoures: devoures, drawers: drawers);
 
                 var futureValueForCards = rotations * totalCardValueForRotation;
@@ -59,11 +87,11 @@ namespace UnderdarkAI.AI.TargetFunctions
                 futureScore[color] += futureValueForCards;
             }
 
-            foreach (var (color, value) in futureScore)
-            {
-                currentResults[color] += value;
-            }
+            return futureScore;
+        }
 
+        private static Dictionary<Color, double> TownPresentScoreEstimate(Board board, Turn turn, BaseRotationEstimator estimator)
+        {
             ///Оценка будущего вэлью контроля городов от маны и бонусных VP
             var townFutureScore = board.Players
                 .ToDictionary(
@@ -92,13 +120,345 @@ namespace UnderdarkAI.AI.TargetFunctions
                 }
             }
 
-            foreach (var (color, value) in townFutureScore)
+            return townFutureScore;
+        }
+
+        private class LocationRoundDistance
+        {
+            public Location Location { get; }
+            public double RoundDistance { get; set; }
+            public double PreformedDistance { get; private set; }
+            public bool MinCurrentOrNew(double newValue)
             {
-                currentResults[color] += value;
+                if (PreformedDistance > newValue)
+                {
+                    PreformedDistance = newValue;
+                    return true;
+                }
+
+                return false;
+            }
+            public LocationRoundDistance(Location location)
+            {
+                Location = location;
+
+                Reset();
             }
 
+            internal void Reset()
+            {
+                RoundDistance = double.PositiveInfinity;
+                PreformedDistance = double.PositiveInfinity;
+            }
+        }
 
-            return VPScoreTargetFunction.GetDifferenceWithClosestOpponent(currentResults, board, turn);
+        private class LocationCityFutureImpact 
+        {
+            public Location Location { get; }
+            public double RoundsToControl { get; set; }
+            public double RoundsToFullControl { get; set; }
+            public bool IsControlUsed { get; set; }
+            public bool IsFullControlUsed { get; set; }
+            public LocationCityFutureImpact(Location location)
+            {
+                Location = location;
+                Reset();
+            }
+
+            public double GetClosestImpact(double turnLeft)
+            {
+                if (!IsControlUsed)
+                {
+                    if (RoundsToControl < turnLeft)
+                    {
+                        return (turnLeft - RoundsToControl) * 0.4d;
+                    }
+                    else
+                    {
+                        return 0.0d;
+                    }
+                }
+
+                if (!IsFullControlUsed)
+                {
+                    if ((RoundsToFullControl - RoundsToControl) < turnLeft)
+                    {
+                        return (turnLeft - (RoundsToFullControl - RoundsToControl)) * Location.BonusVP;
+                    }
+                    else
+                    {
+                        return 0.0d;
+                    }
+                }
+
+                return 0.0d;
+            }
+
+            public void Reset()
+            {
+                RoundsToControl = double.PositiveInfinity;
+                RoundsToFullControl = double.PositiveInfinity;
+                IsControlUsed = false;
+                IsFullControlUsed = false;
+            }
+        }
+
+        private Dictionary<Color, double> TownFutureScoreEstimate(Board board, Turn turn, BaseRotationEstimator estimator)
+        {
+            ///Оценка будущего вэлью контроля городов от маны и бонусных VP
+            var townFutureScore = board.Players
+                .ToDictionary(
+                    kv => kv.Key,
+                    kv => 0.0d
+                    );
+
+            var turnLeft = estimator.RoundLeftEstimator(board, turn);
+
+            var locationDistances = board
+                .Locations
+                .ToDictionary(
+                    l => l,
+                    l => new LocationRoundDistance(l)
+                );
+
+            foreach (var color in turn.AllPlayers)
+            {
+                var player = board.Players[color];
+
+                if (color == Color.YELLOW)
+                {
+                    int y = 1;
+                }
+
+                var totalCards = player.Hand
+                    .Concat(player.Deck)
+                    .Concat(player.Discard)
+                    .ToList();
+
+                double handSize = 5.0d;
+
+                var whiteDisplacementsPerRound = totalCards
+                    .Average(c => context.CardsStatsDict[c.SpecificType].WhiteDisplacement)
+                    * handSize;
+                var colorDisplacementsPerRound = totalCards
+                    .Average(c => context.CardsStatsDict[c.SpecificType].ColorDisplacement)
+                    * handSize;
+                var deploymentsPerRound = totalCards
+                    .Average(c => context.CardsStatsDict[c.SpecificType].DeploySpeed)
+                    * handSize;
+                var spiesPerRound = totalCards.Average(
+                    c => CardMapper.SpecificTypeCardMakers[c.SpecificType].CardType == CardType.GUILE ? 1 : 0
+                    ) * handSize;
+
+                var roundsToWD = whiteDisplacementsPerRound > 0.0d ? 1 / whiteDisplacementsPerRound : double.PositiveInfinity;
+                var roundsToCD = colorDisplacementsPerRound > 0.0d ? 1 / colorDisplacementsPerRound : double.PositiveInfinity;
+                var roundsToDP = deploymentsPerRound > 0.0d ? 1 / deploymentsPerRound : double.PositiveInfinity;
+                var roundsToSP = spiesPerRound > 0.0d ? 1 / spiesPerRound : double.PositiveInfinity;
+
+                foreach (var (location, distanceInfo) in locationDistances)
+                {
+                    if (location.IsPresence(color))
+                    {
+                        distanceInfo.RoundDistance = 0.0d;
+                    }
+                }
+
+                /// Инициализируем нулями точки, где есть присутствие
+                foreach (var (location, distanceInfo) in locationDistances)
+                {
+                    if (location.IsPresence(color))
+                    {
+                        distanceInfo.RoundDistance = 0.0d;
+                    }
+                }
+
+                int posInfCountBefore = locationDistances.Count(d => double.IsPositiveInfinity(d.Value.RoundDistance));
+
+                /// Ищем число ходов, чтобы добраться (получить присутствие) во всех точек на карте
+                while (true)
+                {
+                    /// Идем по непроверенным точкам
+                    foreach (var (location, distanceInfo) in locationDistances)
+                    {
+                        //if (!double.IsPositiveInfinity(distanceInfo.RoundDistance))
+                        //{
+                        //    continue;
+                        //}
+
+                        if (location.Id == LocationId.Phaerlin)
+                        {
+                            int y = 1;
+                        }
+
+                        locationDistances[location].MinCurrentOrNew(roundsToSP);
+
+                        foreach (var neighboor in location.Neighboors)
+                        {
+                            if (neighboor.Id == LocationId.Phaerlin2Everfire)
+                            {
+                                int z = 1;
+                            }
+
+                            if (double.IsPositiveInfinity(locationDistances[neighboor].RoundDistance))
+                            {
+                                continue;
+                            }
+
+                            if (neighboor.FreeSpaces > 0)
+                            {
+                                locationDistances[location].MinCurrentOrNew(
+                                    locationDistances[neighboor].RoundDistance + roundsToDP
+                                    );
+                            }
+                            else
+                            {
+                                if (neighboor.Troops[Color.WHITE] > 0)
+                                {
+                                    locationDistances[location].MinCurrentOrNew(
+                                        locationDistances[neighboor].RoundDistance + Math.Max(roundsToDP, roundsToWD)
+                                        );
+                                }
+
+                                if (neighboor.Troops.Any(kv => kv.Key != color && kv.Key != Color.WHITE && kv.Value > 0))
+                                {
+                                    locationDistances[location].MinCurrentOrNew(
+                                        locationDistances[neighboor].RoundDistance + Math.Max(roundsToDP, roundsToCD)
+                                        );
+                                }
+                            }
+                        }
+                    }
+
+                    foreach (var (location, distanceInfo) in locationDistances)
+                    {
+                        //if (double.IsPositiveInfinity(distanceInfo.RoundDistance)
+                        //    && !double.IsPositiveInfinity(distanceInfo.PreformedDistance)
+                        //    )
+                        //{
+                        //    distanceInfo.RoundDistance = distanceInfo.PreformedDistance;
+                        //}
+                        if (distanceInfo.RoundDistance > distanceInfo.PreformedDistance)
+                        {
+                            distanceInfo.RoundDistance = distanceInfo.PreformedDistance;
+                        }
+                    }
+
+                    int posInfCountAfter = locationDistances.Count(d => double.IsPositiveInfinity(d.Value.RoundDistance));
+
+                    if (posInfCountAfter == posInfCountBefore)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        posInfCountBefore = posInfCountAfter;
+                    }
+                }
+
+                /// Идем по городам
+                var cityDistancesForFull = new Dictionary<Location, LocationCityFutureImpact>();
+
+                foreach (var (location, distanceInfo) in locationDistances)
+                {
+                    if (location.BonusVP == 0)
+                    {
+                        continue;
+                    }
+
+                    cityDistancesForFull.Add(location, new LocationCityFutureImpact(location));
+                }
+
+                foreach (var (location, distanceInfo) in cityDistancesForFull)
+                {
+                    int whiteToDispForFull = location.Troops[Color.WHITE];
+                    int colorToDispForFull = location.Troops
+                        .Where(kv => kv.Key != color && kv.Key != Color.WHITE)
+                        .Sum(kv => kv.Value);
+                    int spiesToDispForFull = location.Spies
+                        .Count(kv => kv.Key != color);
+                    int troopsDeployForFull = location.Size - location.Troops[color];
+
+                    var times = new List<double>(4)
+                    {
+                        whiteToDispForFull * roundsToWD,
+                        colorToDispForFull * roundsToCD,
+                        spiesToDispForFull * 0, /// TODO
+                        troopsDeployForFull * roundsToDP,
+                    };
+
+                    var roundToGetFullControl = locationDistances[location].RoundDistance + times.Max();
+
+                    distanceInfo.RoundsToFullControl = roundToGetFullControl;
+                }
+
+                foreach (var (location, distanceInfo) in cityDistancesForFull)
+                {
+                    int halfSize = (int)Math.Ceiling((location.Size + 0.1d) / 2.0d);
+
+                    int whiteToDispForControl = 0;
+                    int colorToDispForControl = 0;
+
+                    if (location.FreeSpaces + location.Troops[color] < halfSize)
+                    {
+                        int toDisplace = halfSize - (location.FreeSpaces + location.Troops[color]);
+
+                        whiteToDispForControl = Math.Min(location.Troops[Color.WHITE], toDisplace);
+                        colorToDispForControl = Math.Min(location.Troops
+                            .Where(kv => kv.Key != color && kv.Key != Color.WHITE)
+                            .Sum(kv => kv.Value), toDisplace);
+                    }
+
+                    int troopsDeployForControl = halfSize > location.Troops[color] ? halfSize - location.Troops[color] : 0;
+
+                    var times = new List<double>(4)
+                    {
+                        whiteToDispForControl * roundsToWD,
+                        colorToDispForControl * roundsToCD,
+                        troopsDeployForControl * roundsToDP,
+                    };
+
+                    var roundToGetFullControl = locationDistances[location].RoundDistance + times.Max();
+
+                    distanceInfo.RoundsToControl = roundToGetFullControl;
+                }
+
+                double currentTurnLeft = turnLeft;
+
+                while (currentTurnLeft > 0)
+                {
+                    var locationFIs = cityDistancesForFull
+                        .Select(kv => kv.Value)
+                        .Where(kv => !kv.IsControlUsed || !kv.IsFullControlUsed)
+                        .ToList();
+
+                    if (locationFIs.Count == 0)
+                    {
+                        break;
+                    }
+
+                    var locationFI = locationFIs.ArgMax(c => c.GetClosestImpact(currentTurnLeft));
+
+                    townFutureScore[color] += locationFI.GetClosestImpact(currentTurnLeft);
+
+                    if (!locationFI.IsControlUsed)
+                    {
+                        locationFI.IsControlUsed = true;
+                        currentTurnLeft -= locationFI.RoundsToControl;
+                    }
+                    else
+                    {
+                        locationFI.IsFullControlUsed = true;
+                        currentTurnLeft -= (locationFI.RoundsToFullControl - locationFI.RoundsToControl);
+                    }
+                }
+
+                foreach (var (_, info) in locationDistances)
+                {
+                    info.Reset();
+                }
+            }
+
+            return townFutureScore;
         }
     }
 }
